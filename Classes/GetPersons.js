51d06d9d9daab9ea36243b5934f083ed6a4af055
Viktor Rangelov <viktor.rangelov2@gmail.com>
2023-02-12 22:05:05 +0200

    Classes, constructor, getter,setter, static

---
+++ Classes/GetPersons.js
@@ -0,0 +1,24 @@
+function givePersons() {
+    class Person{
+        constructor(firstName,lastName,age,email){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+            
+        }
+        toString(){
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`; 
+         }
+        
+    }
+    let person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    let person2 = new Person('SoftUni');
+    let person3 = new Person(_, 'Johnson', 25, _);
+    let person4 = new Person('Gabriel', 'Peterson', 22, 'g.p@gmail.com');
+
+    let personsArr = [person1,person2,person3,person4];
+
+  return personsArr;
+}
+
